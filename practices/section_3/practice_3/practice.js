9ee66c357c6827a9ad296cd8844f8cec3fb1d930
zhangge1998 <1263061442@qq.com>
2017-10-11 18:45:03 +0800

    finish section3

--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -1,6 +1,34 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  let result = getCountSameElements(collection_a);
+  for (let  i of result) {
+    isExit(i, object_b);
+  }
+  return result;
+}
 
+function getCountSameElements(collectionA) {
+  let result = [];
+  for (let i of collectionA) {
+    finditemStatus(i, result);
+  }
+  return result;
+}
+
+function finditemStatus(item, result) {
+  for (let j of result) {
+    if (item.charAt(0) === j.key) {
+      j.count += 1;
+      return;
+    }
+  }
+  result.push({key: item.charAt(0), count: 1});
+}
+
+function isExit(element, objectB) {
+  for (let j of objectB.value)
+    if (element.key === j)
+      element.count -= parseInt(element.count / 3);
 }
 
 module.exports = create_updated_collection;
--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,41 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var arrayResult = countSameElements(collection_a);
+  for (let i of arrayResult) {
+    isExit(i, object_b);
+  }
+  return arrayResult;
+}
+
+function countSameElements(collection_a) {
+  var result = [];
+  for (let i of collection_a) {
+    findItemStatus(i, result);
+  }
+  return result;
+}
+
+function findItemStatus(item, result) {
+  for (let j of result) {
+    if (item.charAt(0) === j.key) {
+      j.count += elementCount(item);
+      return;
+    }
+  }
+  result.push({key: item.charAt(0), count: elementCount(item)});
+}
+
+function elementCount(item) {
+  if (item.length === 1)
+    return 1;
+  else
+    return parseInt(item.charAt(2));
+}
+
+function isExit(element, object_b) {
+  for (let j of object_b.value)
+    if (element.key === j)
+      element.count -= parseInt(element.count / 3);
 }
 
 module.exports = create_updated_collection;
