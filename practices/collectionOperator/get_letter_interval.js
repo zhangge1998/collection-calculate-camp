368872d1f35dde316586942867b5d39bcd4aed64
zhangge1998 <1263061442@qq.com>
2017-10-11 19:20:04 +0800

    字母映射

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,7 +2,21 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
-
+  let result=[];
+  if(number_a<number_b){
+    for(let i=number_a;i<=number_b;i++){
+      result.push(String.fromCharCode(i+96));
+    }
+  }
+  else if(number_a>number_b){
+    for(let j=number_a;j>=number_b;j--){
+      result.push(String.fromCharCode(j+96));
+    }
+  }
+  else{
+    result.push(String.fromCharCode(number_a+96));
+  }
+  return result;
 }
 
 module.exports = get_letter_interval;
